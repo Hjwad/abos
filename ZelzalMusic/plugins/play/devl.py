77d0e8c1a27f43cf218da6b9e1ad0b39c6d9b438
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-03-20 07:03:47 +0300

    Update devl.py

--- ZelzalMusic/plugins/play/devl.py
+++ ZelzalMusic/plugins/play/devl.py
@@ -4,7 +4,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
-from AarohiX import app
+from ZelzalMusic import app
 import config
 
 @app.on_message(
