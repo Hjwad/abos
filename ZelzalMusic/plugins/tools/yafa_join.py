cfcdb7a4b54530edc3527ea4fd5ae40b5844c613
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-04-08 23:54:41 +0300

    Update yafa_join.py

--- ZelzalMusic/plugins/tools/yafa_join.py
+++ ZelzalMusic/plugins/tools/yafa_join.py
@@ -4,7 +4,7 @@ from config import YAFA_CHANNEL, YAFA_NAME, CHANNEL_SUDO
 from ZelzalMusic import app
 
 
-@app.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
+@app.on_message(~filter.edited & filter.incoming & filter.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not YAFA_CHANNEL:  # Not compulsory
         return
